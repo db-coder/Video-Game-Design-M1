862943c42259f0891ab8e0120fa9775503147c2b
db-coder <dibyendumondal14@gmail.com>
2017-09-30 12:58:31 -0400

    Mileston 1 completed

---
+++ Assets/Scripts/footstep_script.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class footstep_script : MonoBehaviour
+{
+
+    private Animator anim;
+    private AudioSource mySound;
+    public static GameObject floor;
+
+    private float currentFrameFootstepLeft;
+    private float currentFrameFootstepRight;
+    private float lastFrameFootstepLeft;
+    private float lastFrameFootstepRight;
+
+    [Space(5.0f)]
+    private float currentVolume;
+    [Range(0.0f, 1.0f)]
+    public float volume = 1.0f;
+    [Space(5.0f)]
+    public GameObject leftFoot ;         //Drag your player's RIG/MESH/BIP/BONE for the left foot here, in the inspector.
+    public GameObject rightFoot;        //Drag your player's RIG/MESH/BIP/BONE for the right foot here, in the inspector.
+    [Space(5.0f)]
+    public AudioClip defaults = new AudioClip();
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+        mySound = gameObject.GetComponent<AudioSource>();
+        //leftFoot = anim.transform.Find("mixamorig:Hips/mixamorig:LeftUpLeg/mixamorig:LeftLeg/mixamorig:LeftFoot");
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        currentFrameFootstepLeft = anim.GetFloat("FootstepLeft");
+        if(currentFrameFootstepLeft > 0 && lastFrameFootstepLeft < 0)
+        {
+            RaycastHit surfaceHitLeft;
+            Ray aboveLeftFoot = new Ray(leftFoot.transform.position + new Vector3(0, 1.5f, 0), Vector3.down);
+            if(Physics.Raycast(aboveLeftFoot, out surfaceHitLeft, 2f))
+            {
+                floor = surfaceHitLeft.transform.gameObject;
+                if (floor != null)
+                    Invoke("PlaySound", 0);
+            }
+        }
+        lastFrameFootstepLeft = anim.GetFloat("FootstepLeft");
+
+        currentFrameFootstepRight = anim.GetFloat("FootstepRight");
+        if (currentFrameFootstepRight > 0 && lastFrameFootstepRight < 0)
+        {
+            RaycastHit surfaceHitRight;
+            Ray aboveRightFoot = new Ray(rightFoot.transform.position + new Vector3(0, 1.5f, 0), Vector3.down);
+            if (Physics.Raycast(aboveRightFoot, out surfaceHitRight, 2f))
+            {
+                floor = surfaceHitRight.transform.gameObject;
+                if (floor != null)
+                    Invoke("PlaySound", 0);
+            }
+        }
+        lastFrameFootstepRight = anim.GetFloat("FootstepRight");
+    }
+
+    void PlaySound()
+    {
+        currentVolume = Random.Range(0.3f, 1.0f);
+        mySound.pitch = 0.1f;
+        mySound.PlayOneShot(defaults, currentVolume);
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+#Mileston 1
+
+Name: Dibyendu Mondal
+email: dibyendu@gatech.edu
+TSquare account name: dmondal6
+
+Completed Requirements:
+
+Added my name to the 2D HUD.
+Created a 3D character model in Fuse and downloaded in FBX format from maximo server.
+Implemented Mechanim Animation Controller with Walking, Running, Turning, Falling, Throwing and Idle animations.
+Created blend trees for all these animations.
+Implemented falling animation as stated in problem statement.
+Implemented footsteps sound using animation curves method.
+Added a throwing animation layer for blending of locomotion and throwing animation using avatar mask. This is along with an unmasked throw animation state which has transitions from idle state.
+i.e. all requirements are completed.
+
+Resources used:
+1. Footstep sound from Unity Asset Store.
+2. Tutorials for footstep sounds:
+	i. https://www.youtube.com/watch?v=ISoBKFxQLic
+	ii. https://www.youtube.com/watch?v=ih8gyGeC7xs
+3. Tutorial for Animation layers and masks:
+	i. https://www.youtube.com/watch?v=-SAnr8-Xfgg
+
+Main scene file: demo.unity
